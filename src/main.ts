8e157cf064f14b04e3cbc8f8e61ebb2b1aef06df
wildfirecode <wildfirecode13@gmail.com>
2018-10-16 12:40:33 +0800

    1

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+export default class Main extends egret.DisplayObjectContainer {
+    constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    onAddToStage(event) {
+        let textfield = new egret.TextField();
+        this.addChild(textfield);
+        textfield.textColor = 0xff0000;
+        textfield.text = 'hello world';
+    }
+}
+
+
+window['Main'] = Main;
+
+import runEgret from "./runEgret";
+runEgret();
